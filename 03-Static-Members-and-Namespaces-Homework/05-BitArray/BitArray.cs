6769100987b742bcddc7336b1cf9d582de409b80
dinkoslav <dinkoslav@gmail.com>
2015-01-07 11:34:43 +0200

    Few Homeworks

---
+++ 03-Static-Members-and-Namespaces-Homework/05-BitArray/BitArray.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05_BitArray
+{
+    class BitArray
+    {
+        public const int BitCounts = 100000;
+        private BigInteger bitValues;
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= BitCounts)
+                {
+                    throw new IndexOutOfRangeException("Index must be between 0 and 100000!");
+                }
+
+                if ((bitValues & (1 << index)) == 0)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+
+            set
+            {
+                if (index < 0 || index >= BitCounts)
+                {
+                    throw new IndexOutOfRangeException("Index must be between 0 and 100000!");
+                }
+                if (value != 0 && value != 1)
+                {
+                    throw new ArgumentException("Value must be 0 or 1!");
+                }
+
+                bitValues &= ~((BigInteger)(1 << index));
+                bitValues |= (BigInteger)(value << index);
+            }
+        }
+
+        public BigInteger ConvertBinaryToDecimal()
+        {
+            BigInteger decimalNum = 0;
+            int position = 0;
+
+            for (int i = BitCounts - 1; i >= 0; i--)
+            {
+                int bit = this[position];
+                decimalNum += (BigInteger)(this[position] * Math.Pow(2, position));
+                position++;
+            }
+            Console.WriteLine(bitValues);
+            return decimalNum;
+        }
+
+        public override string ToString()
+        {
+            return bitValues.ToString();    
+        }
+    }
+}
---
+++ 03-Static-Members-and-Namespaces-Homework/05-BitArray/test.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05_BitArray
+{
+    public class Test
+    {
+        private static void Main()
+        {
+            BitArray bits = new BitArray();
+
+            bits[0] = 1;
+            bits[3] = 1;
+            bits[8] = 1;
+            bits[900] = 0;
+
+            //Console.WriteLine(bits.ConvertBinaryToDecimal());
+
+            for (int i = 0; i < 901; i++)
+            {
+                Console.Write(bits[i] + " ");
+            }
+        }
+    }
+}
