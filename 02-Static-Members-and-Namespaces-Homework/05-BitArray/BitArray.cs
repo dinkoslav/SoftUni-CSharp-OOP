a44153b375b5d14c3a8923c957ea0abc7406da9c
dinkoslav <dinkoslav@gmail.com>
2015-01-22 18:53:47 +0200

    Full static members

--- 02-Static-Members-and-Namespaces-Homework/05-BitArray/BitArray.cs
+++ 02-Static-Members-and-Namespaces-Homework/05-BitArray/BitArray.cs
@@ -1,66 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace _05_BitArray
-{
-    class BitArray
+public class BitArray
     {
-        public const int BitCounts = 100000;
-        private BigInteger bitValues;
-
-        public int this[int index]
+    private bool[] arr;
+    private int size;
+    public BitArray(int size)
         {
-            get
+        this.Size = size;
+        this.Arr = new bool[size];
+        }
+    public bool[] Arr
+        {
+        get { return this.arr; }
+        set
             {
-                if (index < 0 || index >= BitCounts)
-                {
-                    throw new IndexOutOfRangeException("Index must be between 0 and 100000!");
-                }
-
-                if ((bitValues & (1 << index)) == 0)
-                {
-                    return 0;
-                }
-                else
-                {
-                    return 1;
-                }
-            }
-
-            set
-            {
-                if (index < 0 || index >= BitCounts)
-                {
-                    throw new IndexOutOfRangeException("Index must be between 0 and 100000!");
-                }
-                if (value != 0 && value != 1)
-                {
-                    throw new ArgumentException("Value must be 0 or 1!");
-                }
-
-                bitValues &= ~((BigInteger)(1 << index));
-                bitValues |= (BigInteger)(value << index);
+            if (null == value) throw new ArgumentNullException();
+            this.arr = value;
             }
         }
-
-        public BigInteger ConvertBinaryToDecimal()
+    public int Size
         {
-            BigInteger decimalNum = 0;
-            int position = 0;
-
-            for (int i = BitCounts - 1; i >= 0; i--)
+        get { return this.size; }
+        private set
             {
-                int bit = this[position];
-                decimalNum += (BigInteger)(this[position] * Math.Pow(2, position));
-                position++;
+            if (value < 0) throw new ArgumentException("Invalid value for the array size");
+            this.size = value;
             }
-            Console.WriteLine(bitValues);
-            return decimalNum;
         }
-
-        public override string ToString()
+    public bool this[int index]
         {
-            return bitValues.ToString();    
+        get
+            {
+            if (index < 0 || index > 100000) throw new ArgumentException("The given index is outside the allowed range [0 - 100000]");
+            return this.Arr[index];
+            }
+        set
+            {
+            if (index < 0 || index > 100000) throw new ArgumentException("The given index is outside the allowed range [0 - 100000]");
+            if (value != false && value != true) throw new ArgumentException("The given value is not a valid bit value");
+            this.Arr[index] = value;
+            }
+        }
+    /// <summary>
+    /// Place ones at the positions passed as arguments
+    /// </summary>
+    /// <param name="positions"></param>
+    public void SetBits(params int[] positions)
+        {
+        for (int i = 0; i < positions.Length; i++)
+            {
+            this[positions[i]] = true;
+            }
+        }
+    public override string ToString()
+        {
+        return ArrayToBigNumber().ToString();
+        }
+    private BigInteger ArrayToBigNumber()
+        {
+        BigInteger num = BigInteger.Zero;
+        BigInteger exp2 = BigInteger.One;
+        for (int pos = 0; pos < this.Size; pos++)
+            {
+            exp2 *= 2;
+            if (this[pos])
+                {
+                num += exp2 / 2;
+                }
+            }
+        return num;
         }
     }
-}
